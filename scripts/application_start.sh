c66de38fd589f2a7acfd83fa0610759e43032996
Vivek Richhariya <meettovicky@gmail.com>
2022-10-27 06:55:37 +0530

    test auto deploy5

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo 'run application_start.sh: ' >> /home/ec2-user/nodeapp-aws/deploy.log
 echo 'pm2 restart nodeapp-aws' >> /home/ec2-user/nodeapp-aws/deploy.log
-pm2 restart nodeapp-aws >> /home/ec2-user/nodeapp-aws/deploy.log
+sudo pm2 restart nodeapp-aws >> /home/ec2-user/nodeapp-aws/deploy.log
