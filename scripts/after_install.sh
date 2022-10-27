a7b8392f03351349fd7e71afec0bb5d125076835
Vivek Richhariya <meettovicky@gmail.com>
2022-10-27 06:49:08 +0530

    test auto deploy4

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -4,7 +4,7 @@ sudo chmod -R 777 /home/ec2-user/nodeapp-aws
 echo 'run after_install.sh: ' >> /home/ec2-user/nodeapp-aws/deploy.log
 
 echo 'cd /home/ec2-user/nodejs-server-cicd' >> /home/ec2-user/nodeapp-aws/deploy.log
-cd /home/ec2-user/nodejs-aws-codedeploy-pipeline >> /home/ec2-user/nodeapp-aws/deploy.log
+cd /home/ec2-user/nodeapp-aws >> /home/ec2-user/nodeapp-aws/deploy.log
 
 echo 'npm install' >> /home/ec2-user/nodeapp-aws/deploy.log 
 npm install >> /home/ec2-user/nodeapp-aws/deploy.log
