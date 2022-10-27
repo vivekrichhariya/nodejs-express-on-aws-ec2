91facc3ddf03b76f73ac2a47ec962337e8a8c0de
Vivek Richhariya <meettovicky@gmail.com>
2022-10-27 06:46:15 +0530

    test auto deploy3

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+sudo chmod -R 777 /home/ec2-user/nodeapp-aws
+
 echo 'run after_install.sh: ' >> /home/ec2-user/nodeapp-aws/deploy.log
 
 echo 'cd /home/ec2-user/nodejs-server-cicd' >> /home/ec2-user/nodeapp-aws/deploy.log
